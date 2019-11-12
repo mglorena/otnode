4c50d43f565768c35c1f820a2e21850ddf4affd7
mglorena@gmail.com <mglorena@gmail.com>
2019-11-12 20:14:54 -0300

    Funciona el Search

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,11 +11,23 @@ router.get('/contact', (req, res) => {
   res.render('contact', { title: 'Contact Page' });
 });
 
-router.get('/search', async (req, res) => {
-  
-  const result = await pool.query("select UserName,FirstName from Usuarios;");
-  console.log(result);
-  res.render('search',{search:result[0].FirstName});
- 
+function searchOt(inputText)
+{
+  const query = "call ot_search('"+inputText+"','"+inputText+"');"
+  const result =  pool.query(query);
+  return result;
+}
+ router.get('/search', async (req, res) => {
+   let { term } = req.query;
+   const inputText = (!term?'':term);
+   const result = await searchOt(inputText);
+   res.render('search',{search:result[0], title: 'Ofertas Tecnológicas - UNSa' });
+ });
+ router.get('/search/send', async (req, res) => {
+  let { term } = req.query;
+  const inputText = (!term?'':term);
+  const result = await searchOt(inputText);
+  res.render('search',{search:result[0], title: 'Ofertas Tecnológicas - UNSa' });
 });
+
 module.exports = router;
