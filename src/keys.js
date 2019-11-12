c18783de37bc623a19965ca182d8df0b34c8f504
mglorena@gmail.com <mglorena@gmail.com>
2019-11-11 21:04:10 -0300

    Conexion a base de datos

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'localhost',
+        user: 'coope',
+        password: 'oysadmin',
+        database: 'ot'
+    }
+
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../database');
 
 router.get('/', (req, res) => {
   res.render('index', { title: 'Ofertas Tecnológicas - UNSa' });
 });
 
+
 router.get('/contact', (req, res) => {
   res.render('contact', { title: 'Contact Page' });
 });
 
+router.get('/search', async (req, res) => {
+  
+  const result = await pool.query("select UserName,FirstName from Usuarios;");
+  console.log(result);
+  res.render('search',{search:result[0].FirstName});
+ 
+});
 module.exports = router;
