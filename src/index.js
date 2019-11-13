5a1c73d6dbfe61962171d9cad8554b09eb5a1b9d
Lorena Garcia <mglorena@gmail.com>
2019-11-13 13:58:22 -0300

    Añadiendo algo de seguridad

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-
+const helmet = require('helmet');
 // settings
+app.use(helmet());
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
